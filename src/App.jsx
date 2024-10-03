a56ff5177eef6bf5036d6e33008cd6f556023377
YaroslavKyba <narxoc.ne@gmail.com>
2024-10-03 23:59:19 +0300

    start

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Route, Routes } from 'react-router-dom';
+
+import ErrorPage from 'pages/ErrorPage/ErrorPage';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="*" element={<ErrorPage />} />
+    </Routes>
+  );
+}
+export default App;
---
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,11 @@
+import { Container, Title } from './ErrorPage.styled';
+
+const ErrorPage = () => {
+  return (
+    <Container>
+      <Title>Я не працюю</Title>
+    </Container>
+  );
+};
+
+export default ErrorPage;
