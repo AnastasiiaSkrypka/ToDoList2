2786ae4aaf14a934ead8fc0f0feda1abd6bcc960
YaroslavKyba <narxoc.ne@gmail.com>
2024-10-04 03:03:17 +0300

    add store

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 
-import ErrorPage from 'pages/ErrorPage/ErrorPage';
+// import ErrorPage from 'pages/ErrorPage/ErrorPage';
+import Todos from './components/Todos/Todos';
 
 function App() {
   return (
     <Routes>
-      <Route path="*" element={<ErrorPage />} />
+      <Route path="*" element={<Todos />} />
     </Routes>
   );
 }
